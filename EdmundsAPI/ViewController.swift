89a57476fbe7c1d588bc3b6a7195894414e234b0
KusEvi <KusEvi@KusEvis-Mac-Pro.local>
2015-01-28 16:46:39 -0800

    Initial Commit

---
+++ EdmundsAPI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EdmundsAPI
+//
+//  Created by KusEvi on 1/28/15.
+//  Copyright (c) 2015 Frames Per Sound. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
