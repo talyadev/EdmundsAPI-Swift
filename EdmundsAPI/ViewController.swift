e249df349c30b65ae73d878fe455091935dde55a
KusEvi <KusEvi@KusEvis-Mac-Pro.local>
2015-01-31 11:58:20 -0800

    Updated error messages.

--- EdmundsAPI/ViewController.swift
+++ EdmundsAPI/ViewController.swift
@@ -13,16 +13,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // Example usage
-        let manager = EdmundsAPIManager()
-        manager.getAllCarMakes(state: EdmundsAPIManager.STATE.kNEW, year: "2013", view: EdmundsAPIManager.VIEW.kBASIC) { (dictionary: NSDictionary?, error: NSError?) -> Void in
-            if let e = error {
-                println(e)
-            }
-            else {
-                println(dictionary)
-            }
-        }
     }
 
     override func didReceiveMemoryWarning() {
